d285c137cb11b68b4534f4e2ed3a87c6b1023f01
Conor <conorhamilton@icloud.com>
2024-02-16 15:55:02 +0000

    first commit with styling and html

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+// The setup:
+//  This is where I am going to create the canvas for the animated background I want.
+//
+// Creating the shapes: I O T S Z J L
+
+const width = 10;
+
+// define the shapes
+// function to draw the shapes randomly
+//
+// core game mechanics / logic:
+// // start the game
+// creates variables such as score, level, lines cleared highscore etc.
+// create functions for moving and rotating the shapes
+// collision detection for the borders + placement
+// function to update the board and clear complete lines
+//
+//
+// game loop:
+// track time - remember setInterval & clearInterval when game is over.
+// update score
+// store / update highscore
+//
+//
+// controls
+// event listeners for keyboard inputs (left, right, rotation, dropping down)
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
